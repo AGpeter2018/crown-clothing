1347b1d8bd593eab89e21903a740222d71de187c
AGpeter2018 <adenijipeter2018@gmail.com>
2025-07-06 19:56:47 +0100

    updating menu item to navigate using useNavigation function HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage.component";
 
+const HatPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/hats" element={<HatPage />} />
+    </Routes>
   );
 }
 
--- src/components/menu-item.component.jsx
+++ src/components/menu-item.component.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useNavigate, useMatch } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className="background-image"
-    ></div>
-    <div className="content">
-      <div className="title">{title}</div>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  const match = useMatch(""); // You may want to adjust this depending on your routing structure
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      ></div>
+      <div className="content">
+        <div className="title">{title}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default MenuItem;
